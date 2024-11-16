0ff5d78a6759485964cb395bfe4d213f6deac3a8
Sinka <xgao@zoyoe.com>
2024-11-16 12:07:57 +0800

    init commit for cleaner frontend repo

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { sign, query, LeHexBN }  from "./sign.js";
+import { ZKWasmAppRpc } from "./rpc.js";
+import { composeWithdrawParams} from "./convention.js";
+
+export {sign, query, ZKWasmAppRpc, LeHexBN, composeWithdrawParams}
