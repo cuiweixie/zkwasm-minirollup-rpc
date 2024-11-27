a0dfe5151659db68d4df32e6e70096b17eb65fdc
sinka <xgao@zoyoe.com>
2024-11-27 22:34:11 +1100

    split out uiux lib to browser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { sign, query, LeHexBN }  from "./sign.js";
 import { ZKWasmAppRpc } from "./rpc.js";
 import { composeWithdrawParams, PlayerConvention } from "./convention.js";
-import AccountSliceReducer, * as AccountSlice from "./reduxstate.js";
-
-export {sign, query, ZKWasmAppRpc, LeHexBN, composeWithdrawParams, AccountSlice, AccountSliceReducer, PlayerConvention}
+export {sign, query, ZKWasmAppRpc, LeHexBN, composeWithdrawParams, PlayerConvention}
