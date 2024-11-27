296edb59477cdbaf8d85ecd095db78642e372ee7
sinka <xgao@zoyoe.com>
2024-11-27 21:07:49 +1100

    rollback to .js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { sign, query, LeHexBN }  from "./sign";
-import { ZKWasmAppRpc } from "./rpc";
-import { composeWithdrawParams, PlayerConvention } from "./convention";
-import AccountSliceReducer, * as AccountSlice from "./reduxstate";
+import { sign, query, LeHexBN }  from "./sign.js";
+import { ZKWasmAppRpc } from "./rpc.js";
+import { composeWithdrawParams, PlayerConvention } from "./convention.js";
+import AccountSliceReducer, * as AccountSlice from "./reduxstate.js";
 
 export {sign, query, ZKWasmAppRpc, LeHexBN, composeWithdrawParams, AccountSlice, AccountSliceReducer, PlayerConvention}
